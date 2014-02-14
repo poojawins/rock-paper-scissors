a67c8052d12e77cb691284006ea894ac35350db0
Pooja Pasawala <pooja@basicer.com>
2014-02-13 23:41:03 -0500

    Add working image links and update throw.rb logic to include spock and lizard

--- lib/throw.rb
+++ lib/throw.rb
@@ -5,7 +5,7 @@ class Throw
 
   def initialize(user_move)
     @user_move = user_move
-    @comp_move = ["rock", "paper", "scissors"].sample
+    @comp_move = ["rock", "paper", "scissors", "lizard", "spock"].sample
   end
 
   def computer_move
@@ -15,9 +15,11 @@ class Throw
   def who_wins
     winner = nil
     winner_hash = {
-      :rock => {:rock=> "tie", :paper=> "lose", :scissors=> "win"},
-      :paper => {:rock=> "win", :paper=> "tie", :scissors=> "lose"},
-      :scissors => {:rock=> "lose", :paper=> "win", :scissors=> "tie"}
+      :rock => {:rock=> "tie", :paper=> "lose", :scissors=> "win", :lizard=> "win", :spock=> "lose"},
+      :paper => {:rock=> "win", :paper=> "tie", :scissors=> "lose", :lizard=> "lose", :spock=> "win"},
+      :scissors => {:rock=> "lose", :paper=> "win", :scissors=> "tie", :lizard=> "win", :spock=> "lose"},
+      :lizard => {:rock=> "lose", :paper=> "win", :scissors=> "lose", :lizard=> "tie", :spock=> "win"},
+      :spock => {:rock=> "win", :paper=> "lose", :scissors=> "win", :lizard=> "lose", :spock=> "tie"}
     }
     winner_hash.each do |user, comp_hash|
       if @user_move == user
